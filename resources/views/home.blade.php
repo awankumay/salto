ef857a21665e83e8fab8f4084af800f5242d240a
fajar <fajarqibs@gmail.com>
2020-10-17 00:02:31 +0700

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,7 +75,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-xl-3 col-md-6 mb-4">
+<!--         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-danger shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -104,7 +104,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,6 +125,7 @@
                         <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                             <i class="fa fa-bars"></i>
                         </button>
+                        <span class="text-uppercase">Politeknik Ilmu Pemasyarakatan</span>
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item dropdown no-arrow mx-1">
                               <!--   <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
