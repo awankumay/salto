65f6b9f57e2780245dd5419109e9cb84096b8d52
fajar <fajarqibs@gmail.com>
2020-11-24 03:13:32 +0700

    udpated

--- app/Http/Controllers/api/SuketController.php
+++ app/Http/Controllers/api/SuketController.php
@@ -370,7 +370,7 @@ class SuketController extends BaseController
 
         $data['permission'] = [];
         if(($roleName=='Taruna' || $roleName=='Orang Tua') && $getSurat->status_disposisi!=1 ) {
-            if($getSurat->user_created!=$getSurat->id_user){
+            if($getSurat->user_created==$getSurat->id_user){
                 $data['permission'] = ['edit', 'delete'];
             }
         }
