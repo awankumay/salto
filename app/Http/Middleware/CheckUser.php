27bf2e04881b77b3890068efe1848ae0f57a82e5
fajar <fajarqibs@gmail.com>
2020-11-25 03:37:11 +0700

    updated

--- app/Http/Middleware/CheckUser.php
+++ app/Http/Middleware/CheckUser.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\User;
+use Illuminate\Routing\Route;
 class CheckUser
 {
     /**
@@ -13,6 +14,7 @@ class CheckUser
      * @param  \Closure  $next
      * @return mixed
      */
+    
     public function handle($request, Closure $next)
     {
         $response = [
@@ -21,15 +23,28 @@ class CheckUser
             'message' => 'User Or Role Not Found'
         ];
 
-
-        if(!empty($request->id_user) || !empty($request->idUser)){
-            $getUser = User::find($request->id_user);
-            if(empty($getUser)) {
-                return response()->json($response, 500);
+        if ($request->isMethod('post')) {
+            //@dd($request->input('idUser'));
+            if(!empty($request->input('idUser')) || !empty($request->input('id_user'))){
+                $getUser = User::find($request->input('idUser'));
+                if(empty($getUser)) {
+                    return response()->json($response, 500);
+                }
+                $roleName = $getUser->getRoleNames()[0];
+                if(empty($roleName)) {
+                    return response()->json($response, 500);
+                }
             }
-            $roleName = $getUser->getRoleNames()[0];
-            if(empty($roleName)) {
-                return response()->json($response, 500);
+        }else{
+            if(!empty($request->id_user) || !empty($request->idUser)){
+                $getUser = User::find($request->id_user);
+                if(empty($getUser)) {
+                    return response()->json($response, 500);
+                }
+                $roleName = $getUser->getRoleNames()[0];
+                if(empty($roleName)) {
+                    return response()->json($response, 500);
+                }
             }
         }
         return $next($request);
