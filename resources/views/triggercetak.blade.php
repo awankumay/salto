6477aa2620e147ad7fdd66b93ee7979e409d8aa1
fajar <fajarqibs@gmail.com>
2020-11-30 00:42:21 +0700

    updated

--- resources/views/triggercetak.blade.php
+++ resources/views/triggercetak.blade.php
@@ -1,5 +1,7 @@
-<html>
+<!-- <html>
 <body style="background-color: rgb(38, 38, 38); height: 100%; width: 100%; overflow: hidden; margin: 0px;">
 <embed width="100%" height="100%" name="plugin" id="plugin" src="https://salto.tokodagingdepok.com/cetaksurat/LwsCNe" type="application/pdf">
 </body>
-</html>
+</html> -->
+<script src="/js/pdfobject.js"></script>
+<script>PDFObject.embed("https://salto.tokodagingdepok.com/cetaksurat/LwsCNe", "#example1");</script>
